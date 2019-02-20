216079a50ff9cb9f40e954edef1684d56303c8a3
Eagle-Eye2324 <34150984+Eagle-Eye2324@gmail.com>
2019-02-20 13:41:53 -0800

    add template file for ROBOTC and main file for 2018-19

---
+++ Templates/TFRC Template.c
@@ -0,0 +1,120 @@
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                         VEXnet Joystick Weirdness                         */
+/*                                                                           */
+/*	So the joysticks on our VEXnet controller are acting up.  Because of     */
+/*	this, keep the following in mind when progamming it.  Up and down on the */
+/*	right stick is channel #.  Left and right on the right stick is          */
+/*	channel #.  Up and down on the left stick is channel #, and left and     */
+/*	right on the left stick is channel #.  If you are having problems with   */
+/*	controls, make sure that you have the channels set correctly!  If you    */
+/*	mess up, it's entirely your fault!                                       */
+/* 		- Robbie                                                               */
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                             Sensor Notes                                  */
+/*                                                                           */
+/* - Shaft Encoders > 360 counts per revolution (1 count = 1 degree)         */
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*        Description: Competition template for VEX EDR                      */
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+
+// This code is for the VEX cortex platform
+#pragma platform(VEX2)
+
+// Select Download method as "competition"
+#pragma competitionControl(Competition)
+
+//Main competition background code...do not modify!
+#include "Vex_Competition_Includes.c"
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                     Variable and Function Definitions                     */
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+/*                          Pre-Autonomous Functions                         */
+/*                                                                           */
+/*  You may want to perform some actions before the competition starts.      */
+/*  Do them in the following function.  You must return from this function   */
+/*  or the autonomous and usercontrol tasks will not be started.  This       */
+/*  function is only called once after the cortex has been powered on and    */
+/*  not every time that the robot is disabled.                               */
+/*---------------------------------------------------------------------------*/
+
+void pre_auton()
+{
+  // Set bStopTasksBetweenModes to false if you want to keep user created tasks
+  // running between Autonomous and Driver controlled modes. You will need to
+  // manage all user created tasks if set to false.
+  bStopTasksBetweenModes = true;
+
+	// Set bDisplayCompetitionStatusOnLcd to false if you don't want the LCD
+	// used by the competition include file, for example, you might want
+	// to display your team name on the LCD in this function.
+	// bDisplayCompetitionStatusOnLcd = false;
+
+  // All activities that occur before the competition starts
+  // Example: clearing encoders, setting servo positions, ...
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              Autonomous Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous phase of   */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+task autonomous()
+{
+  // ..........................................................................
+  // Insert user code here.
+  // ..........................................................................
+
+  // Remove this function call once you have "real" code.
+  AutonomousCodePlaceholderForTesting();
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              User Control Task                            */
+/*                                                                           */
+/*  This task is used to control your robot during the user control phase of */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+task usercontrol()
+{
+  // User control code here, inside the loop
+
+  while (true)
+  {
+    // This is the main execution loop for the user control program.
+    // Each time through the loop your program should update motor + servo
+    // values based on feedback from the joysticks.
+
+    // ........................................................................
+    // Insert user code here. This is where you use the joystick values to
+    // update your motors, etc.
+    // ........................................................................
+
+    // Remove this function call once you have "real" code.
+    UserControlCodePlaceholderForTesting();
+  }
+}
